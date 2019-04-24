f696dcba848d027468cdaf11657ff8cf6381d281
yohan <becre1776@kaist.ac.kr>
2019-04-25 00:52:49 +0900

    version 1.04

--- main_movielens100k.py
+++ main_movielens100k.py
@@ -2,6 +2,8 @@ from data_manager import *
 from models.SFTRL_CCFM import SFTRL_CCFM
 from models.SFTRL_Vanila import SFTRL_Vanila
 from models.FM_FTRL import  FM_FTRL
+from models.RRF import RRF
+
 from peformance_manager import *
 
 
@@ -13,10 +15,9 @@ if __name__ == "__main__":
     nbRatingsTrain = 90570
     nbRatingsTest = 9430
 
-    data_dir = './Data/ml-100k/'
+    data_dir = './data/ml-100k/'
     #filename1, filename2 = 'ub.base', './ub.test'
     filename1, filename2 = 'ua.base', './ua.test'
-
 
     # load dataset
     _, x_train, y_train, rate_train, timestamp_train = load_dataset(data_dir + filename1, nbRatingsTrain, nbFeatures, nbUsers)
@@ -24,8 +25,27 @@ if __name__ == "__main__":
     #x_train_s, rate_train_s, _ = sort_dataset(x_train, rate_train, timestamp_train)
     x_train_s, rate_train_s, _ = sort_dataset(x_train, rate_train, timestamp_train)
 
+    want_permute = True
+    if want_permute :
+        idx = np.random.permutation(x_train_s.shape[0])
+        x_train_s = x_train_s[idx]
+        rate_train_s = rate_train_s[idx]
+    else:
+        pass
 
-    down_sampling = 20
+
+    down_sampling = 50
+    # sparse to dense
+
+    # # model setup
+
+
+    c = RRF(loss='l2', task="regression", learning_rate=0.01,
+            learning_rate_gamma=0.001, gamma = 1.0, D = 50)
+    pred_RRF = c.fit(x_train_s.todense()[0:x_train_s.todense().size:down_sampling], rate_train_s[0:x_train_s.todense().size:down_sampling])
+
+
+
     # sparse to dense
     inputs_matrix = torch.tensor(x_train_s[0:x_train_s.size:down_sampling].todense()).double()
     outputs = torch.tensor(rate_train_s[0:x_train_s.size:down_sampling]).double()
@@ -69,11 +89,24 @@ if __name__ == "__main__":
     reg_metric_C = regression_metric(pred_C, real)
     reg_metric_V = regression_metric(pred_V, real)
     reg_metric_F = regression_metric(pred_F, real)
+    reg_metric_RRF = regression_metric(pred_RRF, real)
 
-    # save_legend = ['SFTRL_CCFM','SFTRL_Vanila']
-    # fig_prediction([pred_C,pred_V], save_legend,real)
-    # fig_metric([reg_metric_C,reg_metric_V], save_legend )
+    # save_legend = ['SFTRL_CCFM', 'SFTRL_Vanila']
+    # fig_prediction([pred_C,pred_V], save_legend,real,options)
+    # fig_metric([reg_metric_C,reg_metric_V], save_legend,options)
 
-    save_legend = ['SFTRL_CCFM', 'SFTRL_Vanila','FM_FTRL']
-    fig_prediction([pred_C,pred_V,pred_F], save_legend,real,options)
-    fig_metric([reg_metric_C,reg_metric_V,reg_metric_F], save_legend,options)
+    #print(reg_metric_C)
+
+    # save_legend = ['SFTRL_CCFM', 'SFTRL_Vanila','RRF']
+    # fig_prediction([pred_C,pred_V,pred_RRF], save_legend,real,options)
+    # fig_metric([reg_metric_C,reg_metric_V,reg_metric_RRF], save_legend,options)
+
+    save_path1 = './figure_results/movielens/pred_reg_'
+    save_path2 = './figure_results/movielens/metric_reg_'
+    save_legend = ['SFTRL_CCFM', 'SFTRL_Vanila','RRF']
+    fig_prediction([pred_C,pred_V,pred_RRF],save_legend,real,options,save_path1)
+    fig_metric([reg_metric_C,reg_metric_V,reg_metric_RRF], save_legend,options,save_path2)
+
+    # save_legend = ['SFTRL_CCFM', 'SFTRL_Vanila','RRF','FM_FTRL']
+    # fig_prediction([pred_C,pred_V,pred_RRF,pred_F],save_legend,real,options,save_path)
+    # fig_metric([reg_metric_C,reg_metric_V,reg_metric_RRF,reg_metric_F], save_legend,options,save_path)
--- peformance_manager.py
+++ peformance_manager.py
@@ -9,7 +9,7 @@ def regression_metric(pred,real):
     metric = [np.inf]
     for idx,(pred_idx,real_idx) in enumerate(zip(pred,real)):
         val += (pred_idx - real_idx)**2
-        metric.append(1 / (idx + 1) * val)
+        metric.append( (1/(idx + 1) )* val)
     metric = np.asarray(metric).reshape([-1,1])
 
     return metric
@@ -19,7 +19,7 @@ def classfication_metric():
     return
 
 
-def fig_prediction(pred,model_name,real,option):
+def fig_prediction(pred,model_name,real,option,save_path):
     plt.figure(figsize=(10,5))
     for (c_pred,model_name) in zip(pred,model_name):
         plt.plot(c_pred,'.',label = model_name ,alpha = 0.5)
@@ -30,25 +30,66 @@ def fig_prediction(pred,model_name,real,option):
     plt.ylim([0.0 - 0.1 , 6.0 + .1])
     plt.legend()
     plt.title(option['Data'] + ' prediction' + ' eta_' + str(option['eta']))
-    plt.savefig('./Figure/exp_reg_' + time.ctime().replace(' ','_').replace(':','-') +'.png')
+    plt.savefig(save_path + time.ctime().replace(' ','_').replace(':','-') +'.png')
     plt.show()
 
-    print('regression results saved as following path :' + './Figure/exp_reg_' + time.ctime().replace(' ','_').replace(':','-') +'.png ')
+    print('regression results saved as following path :' + save_path + time.ctime().replace(' ','_').replace(':','-') +'.png ')
     return
 
 
-def fig_metric(metric,model_name,option):
+def fig_metric(metric,model_name,option,save_path):
     assert(isinstance(metric,list) and isinstance(model_name,list) )
 
     plt.figure(figsize=(10,5))
     for (c_model_metric,c_model_name) in zip(metric,model_name):
         plt.plot(c_model_metric,label = c_model_name ,alpha = 0.9)
 
+
     plt.legend()
     plt.xlabel('iteration')
     plt.ylabel('mean of accumulated loss')
     plt.title(option['Data'] + ' metric' + ' eta_' + str(option['eta']))
-    plt.savefig('./Figure/metric_reg_' + time.ctime().replace(' ','_').replace(':','-')+'.png')
+    plt.savefig(save_path + time.ctime().replace(' ','_').replace(':','-')+'.png')
     plt.show()
-    print('metric_reg saved as following path :' + './Figure/metric_reg_' + time.ctime().replace(' ','_').replace(':','-') +'.png ')
+    print('metric_reg saved as following path :' + save_path + time.ctime().replace(' ','_').replace(':','-') +'.png ')
+    return
+
+
+
+def fig_prediction2(pred,model_name,real,option,save_path):
+    plt.figure(figsize=(10,5))
+    for (c_pred,model_name) in zip(pred,model_name):
+        plt.plot(c_pred,'.',label = model_name ,alpha = 0.5)
+    plt.plot(real,'k.',label = 'real')
+
+    plt.xlabel('iteration')
+    plt.ylabel('rating')
+    plt.ylim([-5.0 - 0.1 , 5.0 + .1])
+    plt.legend()
+    plt.title(option['Data'] + ' prediction' + ' eta_' + str(option['eta']))
+    plt.savefig(save_path + time.ctime().replace(' ','_').replace(':','-') +'.png')
+    plt.show()
+
+    print('regression results saved as following path :' + save_path + time.ctime().replace(' ','_').replace(':','-') +'.png ')
+    return
+
+
+def fig_metric2(metric,model_name,option, save_path):
+    assert(isinstance(metric,list) and isinstance(model_name,list) )
+
+    plt.figure(figsize=(10,5))
+    for (c_model_metric,c_model_name) in zip(metric,model_name):
+        #plt.plot(c_model_metric,label = c_model_name ,alpha = 0.9)
+        plt.plot(c_model_metric[1:],label = c_model_name ,alpha = 0.9)
+
+    #plt.ylim([-5.0 - 0.1 , 250.0 + .1])
+    #plt.xlim([2000.0 - 0.1 , 12000 + .1])
+
+    plt.legend()
+    plt.xlabel('iteration')
+    plt.ylabel('mean of accumulated loss')
+    plt.title(option['Data'] + ' metric' + ' eta_' + str(option['eta']))
+    plt.savefig(save_path + time.ctime().replace(' ','_').replace(':','-')+'.png')
+    plt.show()
+    print('metric_reg saved as following path :' + save_path + time.ctime().replace(' ','_').replace(':','-') +'.png ')
     return
